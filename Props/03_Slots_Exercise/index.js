30cf72f2a013441592cdfb7ff9c3c7f8f20c49d5
Himanshu Mishra <mhimanshu712@gmail.com>
2020-03-06 22:31:25 +0530

    Files Added

---
+++ Props/03_Slots_Exercise/index.js
@@ -0,0 +1,24 @@
+// How to destructure props
+// {s1,s2,s3} = this.props
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Slot Machines!</h1>
+        <Machine
+          s1="🍒"
+          s2="🍒"
+          s3="🍒"
+        />
+        <Machine
+          s1="🍒"
+          s2="🍒"
+          s3="🍊"
+        />
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
---
+++ Props/04_JSX_Loops/index.js
@@ -0,0 +1,20 @@
+// Using map to display an array of data
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Friend
+          name="Elton"
+          hobbies={['Piano', 'Singing', 'Dancing']}
+        />
+        <Friend
+          name="Frida"
+          hobbies={['Drawing', 'Painting']}
+        />
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
