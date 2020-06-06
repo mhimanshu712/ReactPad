3c658e2761874deb0d8522f388ffc63c60d9c241
Himanshu Mishra <mhimanshu712@gmail.com>
2020-06-06 20:59:27 +0530

    Pokedex added

---
+++ 2.Props/serve.bat
@@ -0,0 +1,3 @@
+explorer http://localhost:8080
+python -m SimpleHTTPServer 8080
+
---
+++ Pokedex/pokemon/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Pokedex from './Pokedex'
+
+function App() {
+  return (
+    <div className="App">
+      <Pokedex/>
+    </div>
+  );
+}
+
+export default App;
