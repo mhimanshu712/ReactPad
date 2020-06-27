45b659b8a0e5b586ab6c76d3c0a4a5813629bf60
Himanshu Mishra <mhimanshu712@gmail.com>
2020-06-27 22:14:50 +0530

    CheesyJokes: Title changed :D

--- 16_CheesyJokes/src/JokesList.js
+++ 16_CheesyJokes/src/JokesList.js
@@ -37,10 +37,11 @@ class JokesList extends Component {
                 else 
                 console.log(res.data.joke)
             }
-            this.setState(st => ({
-                jokes: [...jokes,...st.jokes],
-                loading: false
-            }),
+            this.setState(st => {
+                let res = [...jokes,...st.jokes]
+                res = res.sort((a,b)=>b.votes-a.votes)
+                return {loading:false, jokes:res}
+            },
             () => window.localStorage.setItem('jokes',JSON.stringify(this.state.jokes))
             )
     } catch(e){
@@ -75,12 +76,11 @@ class JokesList extends Component {
             )
         }
 
-        let jokes = this.state.jokes.sort((a,b)=>b.votes-a.votes)
         return (
             <div className='JokeList'>
                 <div className='JokeList-sidebar'>
                     <h1 className='JokeList-title'>
-                         <span>Dad</span> Jokes
+                         <span>Bad</span> Jokes
                     </h1>
                     <img
                     src="https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg"></img>
@@ -88,7 +88,7 @@ class JokesList extends Component {
                 </div>
     
                 <div className="JokeList-jokes">
-                    {jokes.map(j => 
+                    {this.state.jokes.map(j => 
                         <Joke
                         key={j.id}
                         votes={j.votes}
