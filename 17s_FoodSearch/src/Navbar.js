cdb4505752b0bd97c701bc0aa0bc9721b0a342ef
Himanshu Mishra <mhimanshu712@gmail.com>
2020-07-02 17:47:22 +0530

    Mini Project done!

--- 17s_FoodSearch/src/Navbar.js
+++ 17s_FoodSearch/src/Navbar.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import {withRouter} from 'react-router-dom'
+import './Navbar.css'
 
 class Navbar extends Component {
     constructor(props) {
@@ -15,10 +16,15 @@ class Navbar extends Component {
         this.props.history.push('/food/salmon')
     }
 
+    handleBack = ()=>{
+        this.props.history.goBack()
+    }
+
     render() {
         return (
-           <div>
+           <div className='Navbar'>
                <button onClick={this.handleLogin}>Log In</button>
+               <button onClick={this.handleBack}>Go Back!</button>
            </div> 
         )
     }
