d093382beda1659cd1d7533c422dfffa026d297f
Himanshu Mishra <mhimanshu712@gmail.com>
2020-07-14 17:16:04 +0530

    MassiveColor: Problem on scaling box

--- 19_MassiveColors/src/ColorBox.js
+++ 19_MassiveColors/src/ColorBox.js
@@ -1,29 +1,36 @@
 import React, { Component } from 'react'
+import {CopyToClipboard} from 'react-copy-to-clipboard'
 import './ColorBox.css'
+
 
 class ColorBox extends Component {
     constructor(props) {
         super(props)
 
         this.state = {
-            
+            copied:false
         }
     }
 
     render() {
         const {name,background} = this.props;
         return (
-            <div style={{backgroundColor:background}} className='ColorBox'>
-                <div className='copy-container'>
-                    <div className='box-content'>
-                        <span>{name}</span>
+            <CopyToClipboard text={background}>
+                <div style={{backgroundColor:background}} className='ColorBox'>
+                    <div style={{backgroundColor:background}} className='copy-overlay' />
+                    <div className='copy-container'>
+                        <div className='box-content'>
+                            <span>{name}</span>
+                        </div>
+                        <button className='copy-button'>Copy</button>
                     </div>
-                    <button className='copy-button'>Copy</button>
+                    <span className='see-more'>More</span>
                 </div>
-                <span className='see-more'>More</span>
-            </div>
+            </CopyToClipboard>
+            
         )
     }
 }
 
 export default ColorBox
+
