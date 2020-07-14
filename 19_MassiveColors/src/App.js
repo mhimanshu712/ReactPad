b6d7e784cc4a88de694cbf60fe58bf9b27549e47
Himanshu Mishra <mhimanshu712@gmail.com>
2020-07-14 16:06:45 +0530

    MassiveColor Project Added

---
+++ 19_MassiveColors/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Palette from './Palette'
+import seedColors from './seedColors'
+
+function App() {
+  return (
+    <div className="App">
+      <Palette {...seedColors[4]}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ 19_MassiveColors/src/Palette.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+
+class Palette extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            
+        }
+    }
+
+    render() {
+        return (
+            <div className='Palette'>
+                {/* navbar */}
+                <div className='Palette-colors'>colorBoxes</div>
+                {/* footer */}
+
+            </div>
+        )
+    }
+}
+
+export default Palette
