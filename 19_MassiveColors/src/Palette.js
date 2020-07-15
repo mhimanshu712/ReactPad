6e0af62ceb20eec3436bd6bc2e5a54bbb3643f31
Himanshu Mishra <mhimanshu712@gmail.com>
2020-07-15 21:46:06 +0530

    MassiveColor: ColorPicker UI Done!

--- 19_MassiveColors/src/Palette.js
+++ 19_MassiveColors/src/Palette.js
@@ -22,10 +22,10 @@ class Palette extends Component {
     }
 
     render() {
-        const {colors} = this.props.palette
+        const {colors,paletteName,emoji} = this.props.palette
         const {level,format} = this.state
         const colorBoxes = colors[level].map(color => (
-            <ColorBox background={color[format]} name={color.name} />
+            <ColorBox background={color[format]} name={color.name} key={color.id}/>
         ))
         return (
             <div className='Palette'>
@@ -37,7 +37,11 @@ class Palette extends Component {
                 <div className='Palette-colors'>
                     {colorBoxes}
                 </div>
-                {/* footer */}
+                
+                <footer className='Palette-footer'>
+                    {paletteName}
+                    <span className='emoji'>{emoji}</span>
+                </footer>
 
             </div>
         )
